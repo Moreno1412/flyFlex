e3a2c185fa60312fd4fdbbcd5e554822d30941b1
Kevin Moreno <78882747+Moreno1412@users.noreply.github.com>
2025-04-21 11:33:03 +0100

    Add files via upload
    Signed-off-by: Kevin Moreno <78882747+Moreno1412@users.noreply.github.com>

--- redirecionador_destinos.js
+++ redirecionador_destinos.js
@@ -1,18 +1,27 @@
 
 function buscarDestino() {
   const destinoInput = document.querySelector('input[placeholder="Destino"]');
-  const origem = "LIS"; // origem fixa para Lisboa
   const destino = destinoInput.value.trim().toLowerCase();
+  const origem = "LIS";  // origem fixa
 
-  const mapaIATA = {'lisboa': 'LIS', 'porto': 'OPO', 'paris': 'CDG', 'berlim': 'BER', 'madrid': 'MAD', 'barcelona': 'BCN', 'roma': 'FCO', 'milao': 'MXP', 'londres': 'LON', 'amsterda': 'AMS', 'viena': 'VIE', 'dublin': 'DUB', 'praga': 'PRG', 'atenas': 'ATH', 'bruxelas': 'BRU', 'oslo': 'OSL', 'copenhaga': 'CPH', 'zurique': 'ZRH', 'genebra': 'GVA', 'helsinque': 'HEL', 'estocolmo': 'STO', 'budapeste': 'BUD', 'warsaw': 'WAW', 'zagreb': 'ZAG', 'sofia': 'SOF', 'bucareste': 'OTP', 'vilnius': 'VNO', 'riga': 'RIX', 'talin': 'TLL', 'reiquiavique': 'REK', 'dubai': 'DXB', 'abu dhabi': 'AUH'};
+  const mapaIATA = {
+    "lisboa": "LIS", "porto": "OPO", "paris": "CDG", "berlim": "BER", "madrid": "MAD", "barcelona": "BCN",
+    "roma": "FCO", "milao": "MXP", "londres": "LON", "amsterda": "AMS", "viena": "VIE", "dublin": "DUB",
+    "praga": "PRG", "atenas": "ATH", "bruxelas": "BRU", "oslo": "OSL", "copenhaga": "CPH", "zurique": "ZRH",
+    "genebra": "GVA", "helsinque": "HEL", "estocolmo": "STO", "budapeste": "BUD", "varsovia": "WAW",
+    "zagreb": "ZAG", "sofia": "SOF", "bucareste": "OTP", "vilnius": "VNO", "riga": "RIX", "talin": "TLL",
+    "reiquiavique": "REK", "dubai": "DXB", "abu dhabi": "AUH"
+  };
 
   const iata = mapaIATA[destino];
-  const baseUrl = "https://tp.media/r?marker=624512&trs=409156&p=8626&campaign_id=121&u=";
 
   if (iata) {
-    const url = baseUrl + encodeURIComponent(`https://www.trip.com/flights/from-${origem}-to-${iata}?locale=pt-pt`);
+    const baseUrl = "https://tp.media/r?marker=624512&trs=409156&p=8626&u=";
+    const destinoTrip = `https://www.trip.com/flights/from-${origem}-to-${iata}?locale=pt-pt&SID=2209817&allianceid=1094387&trip_sub1=e5aeca06d0ff48058421bbd28-624512&utm_campaign=624512`;
+    const url = baseUrl + encodeURIComponent(destinoTrip) + "&campaign_id=121";
+
     window.open(url, "_blank");
   } else {
-    alert("Destino não encontrado. Tente outra cidade ou país.");
+    alert("Destino não encontrado. Tente outro.");
   }
 }
